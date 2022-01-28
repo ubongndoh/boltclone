47989ec7e35377cc0d9efac6702f3021dbd62bb7
Ubong Ndoh <ubongndoh@gmail.com>
2022-01-28 09:30:40 +0100

    set up completed

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import NavBar from './components/NavBar.vue';
+//import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+	<NavBar />
+</template>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	background-color: black;
+}
+</style>
