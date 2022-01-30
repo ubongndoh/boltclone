c32392d538510dc4f6a6ef457287926b645db371
Ubong Ndoh <ubongndoh@gmail.com>
2022-01-30 18:13:06 +0100

    arrange the layout of the homepage

--- src/App.vue
+++ src/App.vue
@@ -3,66 +3,15 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import NavBar from './components/NavBar.vue';
 import Footer from './components/Footer.vue';
+import Main from './components/Main.vue';
 //import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
 	<div>
 		<NavBar />
-		<p
-			>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos id
-			distinctio omnis maxime nostrum obcaecati voluptatem, repellat optio
-			officiis delectus. Beatae soluta commodi odit accusamus, corrupti impedit
-			quisquam dignissimos sequi? Eos, rem quasi, est delectus dolorum labore
-			consectetur eveniet repellat incidunt esse expedita! Officiis tempora
-			veniam, neque excepturi assumenda soluta ipsum explicabo voluptas vel
-			maiores aperiam cupiditate quis ratione ducimus. Tenetur, numquam dolor
-			nam totam ea dolore? Impedit aperiam laboriosam eius omnis odit dolorem,
-			beatae nemo maxime iste quas dolores sequi alias necessitatibus molestias
-			voluptatum labore qui, minima, iure harum. Facere adipisci similique
-			officiis, incidunt molestias expedita molestiae qui pariatur, eos vel
-			voluptates numquam, illo omnis libero quod recusandae nobis consequuntur.
-			Ex, id minus doloribus expedita soluta libero saepe deserunt! Labore
-			distinctio eaque dicta cumque enim officiis soluta eum eius voluptatibus!
-			Accusantium, rerum voluptas blanditiis quos debitis, cupiditate iusto
-			laborum accusamus tenetur sint nisi deleniti nulla est sit! Omnis,
-			tempore?</p
-		><p
-			>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-			veritatis assumenda voluptates illum excepturi commodi tenetur expedita
-			aspernatur nam earum, at rerum obcaecati tempore aut ea animi reiciendis
-			quia. Distinctio? Delectus veritatis eligendi similique neque mollitia.
-			Accusantium nobis recusandae iure culpa? Nemo temporibus ducimus suscipit
-			doloremque quidem dolore, dolor, voluptates cum quaerat officiis error
-			eius, quas accusamus iste illo culpa. Eum voluptates blanditiis, ad culpa
-			aliquid velit facere tenetur sequi voluptatibus. Temporibus unde
-			aspernatur, nobis provident voluptates earum eos assumenda neque natus
-			laudantium voluptatibus facere id modi similique iure corporis. Officia,
-			cum sapiente beatae impedit animi odio dolor consequatur natus hic
-			nesciunt autem sunt. Ipsa, ipsum eius natus suscipit rerum vitae
-			laudantium rem architecto, sint facere illum quibusdam. Consequatur,
-			blanditiis. Quaerat, nemo iste soluta dicta magnam recusandae eligendi ab
-			quis cumque voluptate quia reiciendis, distinctio animi repellendus sint
-			et molestias repudiandae unde harum exercitationem numquam blanditiis
-			dolores quasi ut. Quidem? Magni consequuntur vitae incidunt, minus laborum
-			similique voluptate iusto repudiandae! Sit impedit eos delectus quibusdam
-			omnis doloribus asperiores tempora, earum tenetur quia voluptates error
-			libero ratione! Quasi delectus accusantium id. Iste, quidem vero aliquam
-			animi sit laudantium molestiae nisi, unde quo sunt laborum fuga
-			reprehenderit nihil illum inventore fugiat minima asperiores voluptate
-			dolorem maiores necessitatibus cum. Fugiat, quidem! Ipsam, deleniti? Totam
-			provident autem facilis vitae consequuntur? Animi unde, eum totam saepe
-			itaque quidem velit, impedit assumenda quas quia facilis quo officiis
-			labore, commodi ullam. Voluptatum hic tenetur doloribus provident
-			expedita? Quia officiis ut, quod ab molestiae consectetur, commodi
-			temporibus, ipsa quasi provident illum. Voluptas accusantium quos
-			doloremque exercitationem aspernatur laudantium, eligendi amet blanditiis
-			eveniet ea, molestiae modi assumenda fuga placeat? Reprehenderit similique
-			amet dicta veritatis sequi quos, fugit quibusdam sapiente ad
-			necessitatibus repudiandae ipsum assumenda rem tempora at blanditiis
-			consequuntur! Rerum quaerat veritatis voluptatibus, expedita nisi iusto
-			eveniet quam maiores!</p
-		>
+
+		<Main />
 		<Footer />
 	</div>
 </template>
